179818fb76c75f457c1892578450c0c12ffc4c87
João Pedro Soares do Monte <82397148+JpSoares17@users.noreply.github.com>
2024-12-29 00:43:02 -0300

    Initial Commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,18 @@
+using Unity.Collections;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+        [SerializeField] float xValue = 0f;
+        [SerializeField] float yValue = 0f;
+        [SerializeField] float zValue = 0f;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.Translate(xValue, yValue, zValue);
+    }
+}
